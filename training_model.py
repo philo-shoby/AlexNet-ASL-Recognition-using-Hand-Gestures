843debc24235a284912c8b9afbed7453534c7d64
philo-shoby <56381583+philo-shoby@users.noreply.github.com>
2020-02-27 22:57:11 +0530

    Create training_model.py

---
+++ training_model.py
@@ -0,0 +1,19 @@
+with tf.Session() as sess:
+    # Running Initializer
+    sess.run(init)
+    cost_hist, acc_hist = [], []
+    for epoch in range(1, epochs + 1):
+        _x, _y = next_batch(batch_size, x_train, y_train_encoded)
+        # Running Optimizer
+        sess.run(train_op, feed_dict = { X : _x, Y : _y })
+        if epoch % display_step == 0:
+            # Calculating Loss and Accuracy on the current Epoch
+            loss, acc = sess.run([loss_op, accuracy], feed_dict = { X : _x, Y : _y })
+            loss = loss
+            cost_hist.append(loss)
+            acc_hist.append(acc)
+            print('Epoch ' + str(epoch) + ', Cost: ' + str(loss) + ', Accuracy: ' + str(acc * 100) + ' %')
+    W = sess.run(weights)
+    B = sess.run(biases)
+    print('-' * 70)
+   
